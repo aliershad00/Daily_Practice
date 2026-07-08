288960852c52b17b42c661b94c406a5619c91f9a
Ershad Ali <aliershad056@gmail.com>
2026-07-08 23:22:40 +0530

    feat(js): print multiplication table using for loop

---
+++ 08-07-2026/04_multiplication_table.js
@@ -0,0 +1,9 @@
+// Program: Print Multiplication Table
+
+let number = 7;
+
+console.log("Multiplication Table of " + number);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}
