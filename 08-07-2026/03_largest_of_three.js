d32b1598cf9334663540f6093cd833c679d1cb2a
Ershad Ali <aliershad056@gmail.com>
2026-07-08 23:21:18 +0530

    feat(js): find largest among three numbers

---
+++ 08-07-2026/03_largest_of_three.js
@@ -0,0 +1,13 @@
+// Program: Find Largest Among Three Numbers
+
+let a = 50;
+let b = 90;
+let c = 70;
+
+if (a >= b && a >= c) {
+    console.log("Largest Number:", a);
+} else if (b >= a && b >= c) {
+    console.log("Largest Number:", b);
+} else {
+    console.log("Largest Number:", c);
+}
