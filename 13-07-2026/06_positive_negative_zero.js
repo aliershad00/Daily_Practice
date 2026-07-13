886bc3a571f42fc786441761c2ef0ca514a2f8a9
Ershad Ali <aliershad056@gmail.com>
2026-07-13 23:44:21 +0530

    rogram: Check Positive, Negative or Zero

---
+++ 13-07-2026/06_positive_negative_zero.js
@@ -0,0 +1,11 @@
+// Program: Check Positive, Negative or Zero
+
+let number = -15;
+
+if (number > 0) {
+    console.log(`${number} is Positive`);
+} else if (number < 0) {
+    console.log(`${number} is Negative`);
+} else {
+    console.log("The number is Zero");
+}
